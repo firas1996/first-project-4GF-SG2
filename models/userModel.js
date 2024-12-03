110b23731d6762542e2b488cfb7df4001426f6ae
firas hamila <firas.hm.1996@gmail.com>
2024-12-03 14:51:09 +0100

    aaaa

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,6 +41,13 @@ const userSchema = new mongoose.Schema({
     default: Date.now(),
     select: false,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
